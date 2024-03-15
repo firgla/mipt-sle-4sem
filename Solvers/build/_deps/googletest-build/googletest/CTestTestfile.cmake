b6e18c8de12c604592255cf972099c018f2c34b5
Глафира Фиронова <glafira.fironova@MacBook-Pro-Glafira.local>
2024-03-16 01:40:19 +0300

    build delete

--- Solvers/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/glafira.fironova/Documents/mipt-sle-4sem/Solvers/build/_deps/googletest-src/googletest
-# Build directory: /Users/glafira.fironova/Documents/mipt-sle-4sem/Solvers/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
